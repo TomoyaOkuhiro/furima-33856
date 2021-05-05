b78d3756bc899563b802542ef42c041836d8cdf3
TomoyaOkuhiro <77881067+TomoyaOkuhiro@users.noreply.github.com>
2021-05-05 17:42:12 +0900

    ユーザー管理機能の実装　及びモデルの単体テストコード
    機能ごとにコミットプッシュをするのを忘れてしまいました。申し訳ありません。

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    nickname {'SATO'}
+    last_name {'佐藤'}
+    first_name {'陽二'}
+    last_name_kana {'サトウ'}
+    first_name_kana {'ヨウジ'}
+    birth_day {'2000-01-01'}
+  end
+end
