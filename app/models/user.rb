935e8650a2d5830e5a3a8d4ae9dcb725e00884bc
TomoyaOkuhiro <77881067+TomoyaOkuhiro@users.noreply.github.com>
2021-05-06 21:31:46 +0900

    バリデーションの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, uniqueness: true
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, length: { minimum: 6 } , message: 'Include both letters and numbers' }
   with_options presence: true do
-   validates :nickname
-   validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-   validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-   validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
-   validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
-   validates :birth_day
+      validates :nickname
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+     with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+     end
+      validates :birth_day
   end
 
-  has_many :items
-  has_many :purchase_managements
+      has_many :items
+      has_many :purchase_managements
 end
