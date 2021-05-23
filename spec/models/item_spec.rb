65109a285eb05e7188e38203bd2a1d06083d8b66
TomoyaOkuhiro <77881067+TomoyaOkuhiro@users.noreply.github.com>
2021-05-23 20:37:25 +0900

    出品機能修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -97,7 +97,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'userが紐づいてなければ登録できない' do
-        @item.user = 'nil'
+        @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("User must exist")
       end
