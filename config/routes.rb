3f4911e65edf50e28d746a77ee46903d137490e2
TomoyaOkuhiro <77881067+TomoyaOkuhiro@users.noreply.github.com>
2021-05-30 13:09:50 +0900

    商品詳細機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to:'markets#index'
-  resources :markets, only: [ :index, :new, :create] do
+  resources :markets, only: [ :index, :new, :create, :show, :edit, :destroy ] do
   end
 
 end
