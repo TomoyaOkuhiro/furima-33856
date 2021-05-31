1b7276823e7a523149e112efdca912618719e23b
TomoyaOkuhiro <77881067+TomoyaOkuhiro@users.noreply.github.com>
2021-05-31 21:25:10 +0900

    差分、destroy削除
    route.rbファイルのdestroy記述を削除しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to:'markets#index'
-  resources :markets, only: [ :index, :new, :create, :show, :edit, :update, :destroy ] do
+  resources :markets, only: [ :index, :new, :create, :show, :edit, :update ] do
   end
 
 end
