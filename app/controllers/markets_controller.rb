a0999930bf425611f5915c4ed462e87f38b9372d
TomoyaOkuhiro <77881067+TomoyaOkuhiro@users.noreply.github.com>
2021-05-31 21:00:12 +0900

    商品編集機能実装修正
    beforeアクション
    privateメソッドを使用しedit,updateを編集

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,6 +1,8 @@
 class MarketsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :only_buyer, only: [:edit, :update]
+
   def index
     @items = Item.all.order(id: :DESC)
   end
@@ -19,17 +21,12 @@ class MarketsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == @item.user_id
-
   end
 
   def update
-       @item = Item.find(params[:id])
     if @item.update(item_params)
        redirect_to root_path
     else
@@ -39,5 +36,13 @@ class MarketsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_cost_id, :shipping_area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def only_buyer
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end 
