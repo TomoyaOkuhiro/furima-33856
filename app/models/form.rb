b5f45a021d4a57b1e8f5ad0c6b5cce788e838b14
TomoyaOkuhiro <77881067+TomoyaOkuhiro@users.noreply.github.com>
2021-06-11 15:23:28 +0900

    商品購入機能実装

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,8 +1,4 @@
-class Form
-  include ActiveModel::Model
-  attr_accessor :purchase_management, :post_code, :phone_number, :shipping_area_id, :city, :address, :building_name, :user_id, :item_id, :token
 
-  # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
